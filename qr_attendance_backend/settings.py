7fc083b61775a6ad605f530b5a2967351a1c7999
Emmanuel Igbeng <emmanueligbeng01@gmail.com>
2026-04-28 12:11:33 +0100

    fixed qr code display

--- qr_attendance_backend/settings.py
+++ qr_attendance_backend/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
+# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = [
     "localhost",
